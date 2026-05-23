fd7cd576a4521c209272c1755285a9b2ca4f335b
Ken <ahmadsoud63@gmail.com>
2026-05-23 18:35:14 +0300

    add minimal MVVM structure with MainViewModel and basic UI layout in MainWindow.xaml.

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PixelLab.ViewModels;
 
 namespace PixelLab;
 
@@ -18,6 +19,7 @@ public partial class MainWindow : Window
 {
     public MainWindow()
     {
-        InitializeComponent();
+        // InitializeComponent();
+        DataContext = new MainViewModel();
     }
 }
